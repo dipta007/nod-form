6e36736e5f5ad64d60091969a463a94de7660de2
Shubhashis Roy Dipta <iamdipta@gmail.com>
2020-07-21 18:48:20 +0600

    added header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Form from './hooks/Form'
 import { Container, makeStyles, ThemeProvider } from '@material-ui/core'
 import theme from './util/theme'
+import Header from './components/Header/Header';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -13,6 +14,7 @@ function App() {
   const classes = useStyles()
   return (
     <ThemeProvider theme={theme}>
+      <Header />
       <Container maxWidth="md" className={classes.container}>
         <Form />
       </Container>
