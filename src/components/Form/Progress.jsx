060173e0344f948618a43c87da3892392b8c0b0a
Shubhashis Roy Dipta <iamdipta@gmail.com>
2020-07-21 18:51:02 +0600

    formatter

--- src/components/Form/Progress.jsx
+++ src/components/Form/Progress.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { LinearProgress, Typography, makeStyles } from '@material-ui/core'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   margin: {
-    marginBottom: theme.spacing(4),
+    marginBottom: theme.spacing(4)
   }
 }))
 
-function Progress({ progress }) {
+function Progress ({ progress }) {
   const classes = useStyles()
   return (
     <div className={classes.margin}>
-      <Typography variant="h6">Progress: {progress}%</Typography>
-      <LinearProgress variant="determinate" value={progress} />
+      <Typography variant='h6'>Progress: {progress}%</Typography>
+      <LinearProgress variant='determinate' value={progress} />
     </div>
   )
 }
