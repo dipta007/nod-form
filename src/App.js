ac03ee88145e2ba0178b08cb1a0116e931644396
Shubhashis Roy Dipta <iamdipta@gmail.com>
2020-07-21 14:13:03 +0600

    Added theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Form from './hooks/Form'
+import { Container, makeStyles, ThemeProvider } from '@material-ui/core'
+import theme from './util/theme'
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(8),
+  }
+}))
 
 function App() {
+  const classes = useStyles()
   return (
-    <></>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md" className={classes.container}>
+        <Form />
+      </Container>
+    </ThemeProvider>
   );
 }
 
