0b9a28eeb7b0c6d92a14a0eea3a4e4601803b82b
Shubhashis Roy Dipta <iamdipta@gmail.com>
2020-07-21 18:48:35 +0600

    Added options for select

---
+++ src/util/options.js
@@ -0,0 +1,14 @@
+export const genderOptions = [
+  { value: 'Female', label: 'Female' },
+  { value: 'Male', label: 'Male' },
+  { value: 'Others', label: 'Others' }
+]
+
+export const recommendationOptions = [
+  { value: 'Insurance', label: 'Insurance' },
+  { value: 'Superannuation', label: 'Superannuation' },
+  { value: 'Investments', label: 'Investments' },
+  { value: 'Cashflow', label: 'Cashflow' },
+  { value: 'Pension', label: 'Pension' },
+  { value: 'Estate Planning', label: 'Estate Planning' }
+]
